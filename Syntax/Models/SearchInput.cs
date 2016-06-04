1b8f73e34c4b712740dcbe26e20ce517f89123e8
Kyarie <dora-hu@hotmail.com>
2016-06-04 18:50:34 -0400

    Class

---
+++ Syntax/Models/SearchInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Syntax.Models
+{
+    public class SearchInput
+    {
+        string focus;
+        string context;
+        //File fileName;
+    }
+}
