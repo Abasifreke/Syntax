08fee79966914e6890d9a9d0cb81b6a46514b39e
LarkSetz <LarkSetz@Gmail.Com>
2016-06-05 03:29:22 -0400

    Small stuff
    changes

--- Syntax/Controllers/HomeController.cs
+++ Syntax/Controllers/HomeController.cs
@@ -9,6 +9,20 @@ namespace Syntax.Controllers
 {
     public class HomeController : Controller
     {
+        //[HttpPost]
+        //public ActionResult Index(HttpPostedFileBase file)
+        //{
+
+        //    if (file.ContentLength > 0)
+        //    {
+        //        var fileName = Path.GetFileName(file.FileName);
+        //        var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+        //        file.SaveAs(path);
+        //    }
+
+        //    return RedirectToAction("Index");
+        //}
+
         public ActionResult Index()
         {
             return View();
