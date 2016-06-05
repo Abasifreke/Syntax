c487c7c5c1824995f821b8820d07f7c7a982444d
Abasifreke <jamesabasifreke@yahoo.com>
2016-06-05 05:26:15 -0400

    Building Context Logic
    Context logic code

---
+++ Syntax/Models/ContextLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Syntax.Models
+{
+    //......................................................
+    //Creating skeleton Langtree
+    private class LangTree
+    {
+        private String tag;
+        private String word;
+        private List<LangTree> children;
+
+        public LangTree();
+      
+    }
+    //.......................................................
+
+    public class ContextLogic
+    {
+        
+        public static Boolean langTreeHas(LangTree inputTree, String focusWord)
+        {
+            // Checking if the list of input words contains the focus word. 
+            return inputTree.getWord().contains(focusWord, OrdinalIgnoreCase); 
+               
+        }
+
+        //LangTree inputStringTree = LangTree();
+        //public static String getContext(LangTree inputStringTree, String focusWord)
+        //{
+           
+                   
+        //}
+
+       
+    }
+}
